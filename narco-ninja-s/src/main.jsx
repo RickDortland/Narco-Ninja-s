765ca38bd9a018a539cdbb6dc75e98f45a4b8383
manu4195 <133750445+manu4195@users.noreply.github.com>
2025-04-04 14:23:27 +0200

    Replace Login component with App component and add a custom 404 error page

--- narco-ninja-s/src/main.jsx
+++ narco-ninja-s/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Login from './components/Login'; // Import Login instead of App
+import app from './components/app'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
